af594220b62947de5af7c68a7f92d131ee3281d4
Arturo-Chi <124841476+Arturo-Chi@users.noreply.github.com>
2024-03-04 23:47:05 -0600

    Act1

---
+++ Proyecto_Topicos/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Proyecto_Topicos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel_menu = new System.Windows.Forms.Panel();
+            this.image_up = new System.Windows.Forms.PictureBox();
+            this.botonOpciones = new System.Windows.Forms.Button();
+            this.botont1 = new System.Windows.Forms.Button();
+            this.botont2 = new System.Windows.Forms.Button();
+            this.botont3 = new System.Windows.Forms.Button();
+            this.botont4 = new System.Windows.Forms.Button();
+            this.botont5 = new System.Windows.Forms.Button();
+            this.botont6 = new System.Windows.Forms.Button();
+            this.botonAyuda = new System.Windows.Forms.Button();
+            this.panel_menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.image_up)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_menu
+            // 
+            this.panel_menu.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel_menu.Controls.Add(this.botonAyuda);
+            this.panel_menu.Controls.Add(this.botonOpciones);
+            this.panel_menu.Location = new System.Drawing.Point(-1, 0);
+            this.panel_menu.Name = "panel_menu";
+            this.panel_menu.Size = new System.Drawing.Size(144, 500);
+            this.panel_menu.TabIndex = 0;
+            // 
+            // image_up
+            // 
+            this.image_up.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("image_up.BackgroundImage")));
+            this.image_up.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.image_up.Location = new System.Drawing.Point(279, 29);
+            this.image_up.Name = "image_up";
+            this.image_up.Size = new System.Drawing.Size(481, 86);
+            this.image_up.TabIndex = 1;
+            this.image_up.TabStop = false;
+            // 
+            // botonOpciones
+            // 
+            this.botonOpciones.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("botonOpciones.BackgroundImage")));
+            this.botonOpciones.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.botonOpciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonOpciones.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.botonOpciones.Location = new System.Drawing.Point(29, 31);
+            this.botonOpciones.Name = "botonOpciones";
+            this.botonOpciones.Size = new System.Drawing.Size(75, 46);
+            this.botonOpciones.TabIndex = 0;
+            this.botonOpciones.UseVisualStyleBackColor = true;
+            // 
+            // botont1
+            // 
+            this.botont1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("botont1.BackgroundImage")));
+            this.botont1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.botont1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botont1.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.botont1.Location = new System.Drawing.Point(225, 148);
+            this.botont1.Name = "botont1";
+            this.botont1.Size = new System.Drawing.Size(126, 153);
+            this.botont1.TabIndex = 2;
+            this.botont1.UseVisualStyleBackColor = true;
+            this.botont1.Click += new System.EventHandler(this.botont1_Click);
+            // 
+            // botont2
+            // 
+            this.botont2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("botont2.BackgroundImage")));
+            this.botont2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.botont2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botont2.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.botont2.Location = new System.Drawing.Point(455, 148);
+            this.botont2.Name = "botont2";
+            this.botont2.Size = new System.Drawing.Size(126, 156);
+            this.botont2.TabIndex = 3;
+            this.botont2.UseVisualStyleBackColor = true;
+            // 
+            // botont3
+            // 
+            this.botont3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("botont3.BackgroundImage")));
+            this.botont3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.botont3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botont3.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.botont3.Location = new System.Drawing.Point(677, 151);
+            this.botont3.Name = "botont3";
+            this.botont3.Size = new System.Drawing.Size(126, 156);
+            this.botont3.TabIndex = 4;
+            this.botont3.UseVisualStyleBackColor = true;
+            // 
+            // botont4
+            // 
+            this.botont4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("botont4.BackgroundImage")));
+            this.botont4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.botont4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botont4.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.botont4.Location = new System.Drawing.Point(225, 333);
+            this.botont4.Name = "botont4";
+            this.botont4.Size = new System.Drawing.Size(126, 156);
+            this.botont4.TabIndex = 5;
+            this.botont4.UseVisualStyleBackColor = true;
+            // 
+            // botont5
+            // 
+            this.botont5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("botont5.BackgroundImage")));
+            this.botont5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.botont5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botont5.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.botont5.Location = new System.Drawing.Point(455, 333);
+            this.botont5.Name = "botont5";
+            this.botont5.Size = new System.Drawing.Size(126, 156);
+            this.botont5.TabIndex = 6;
+            this.botont5.UseVisualStyleBackColor = true;
+            // 
+            // botont6
+            // 
+            this.botont6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("botont6.BackgroundImage")));
+            this.botont6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.botont6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botont6.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.botont6.Location = new System.Drawing.Point(677, 333);
+            this.botont6.Name = "botont6";
+            this.botont6.Size = new System.Drawing.Size(126, 156);
+            this.botont6.TabIndex = 7;
+            this.botont6.UseVisualStyleBackColor = true;
+            // 
+            // botonAyuda
+            // 
+            this.botonAyuda.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("botonAyuda.BackgroundImage")));
+            this.botonAyuda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.botonAyuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonAyuda.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.botonAyuda.Location = new System.Drawing.Point(39, 126);
+            this.botonAyuda.Name = "botonAyuda";
+            this.botonAyuda.Size = new System.Drawing.Size(65, 65);
+            this.botonAyuda.TabIndex = 1;
+            this.botonAyuda.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(932, 501);
+            this.Controls.Add(this.botont6);
+            this.Controls.Add(this.botont5);
+            this.Controls.Add(this.botont4);
+            this.Controls.Add(this.botont3);
+            this.Controls.Add(this.botont2);
+            this.Controls.Add(this.botont1);
+            this.Controls.Add(this.image_up);
+            this.Controls.Add(this.panel_menu);
+            this.Name = "Form1";
+            this.Text = "MultiMaster";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_menu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.image_up)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_menu;
+        private System.Windows.Forms.Button botonOpciones;
+        private System.Windows.Forms.PictureBox image_up;
+        private System.Windows.Forms.Button botont1;
+        private System.Windows.Forms.Button botont2;
+        private System.Windows.Forms.Button botont3;
+        private System.Windows.Forms.Button botont4;
+        private System.Windows.Forms.Button botont5;
+        private System.Windows.Forms.Button botont6;
+        private System.Windows.Forms.Button botonAyuda;
+    }
+}
+
